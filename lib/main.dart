6ecfb2f38a7e0d7bd4e73d7fdd241b52fb09aa3d
Player0002 <dudwo0914@gmail.com>
2020-11-12 15:45:56 +0900

    [update] last

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smarthome/provider/fcm_provider.dart';
@@ -11,25 +10,6 @@ import 'package:smarthome/screen/connection_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  _firebaseMessaging.configure(
-    onMessage: (Map<String, dynamic> message) async {
-      print("onMessage: $message");
-    },
-    onLaunch: (Map<String, dynamic> message) async {
-      print("onLaunch: $message");
-    },
-    onResume: (Map<String, dynamic> message) async {
-      print("onResume: $message");
-    },
-  );
-  _firebaseMessaging.requestNotificationPermissions(
-      const IosNotificationSettings(
-          sound: true, badge: true, alert: true, provisional: true));
-  _firebaseMessaging.onIosSettingsRegistered
-      .listen((IosNotificationSettings settings) {
-    print("Settings registered: $settings");
-  });
 
   runApp(
     MultiProvider(
